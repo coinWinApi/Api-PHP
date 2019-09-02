589592d2add2a3084f885abe2dbf0548753e40c4
coinWinApi <54792192+coinWinApi@users.noreply.github.com>
2019-09-02 16:39:17 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -6,7 +6,7 @@ $url='https://xxxxxxx';				//api请求url
 $accessKey='xxxxxxxxxxxxxxxx';     //apiAccessKey
 $secretKey='xxxxxxxxxxxxxxxx';     //apiSecretKey
 
-$reqr=new Pay(,'3BDAF00B6F374A01A4DFBE32257F7E05','1C1D365EEEAB40FD8E56901E27243DA0');
+$reqr=new Pay($url,$accessKey,$secretKey);
 
 
 var_dump($req->getTicker('BTC'));
